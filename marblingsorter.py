ea9590a880d62c05686795dd447c1697f84fd3cc
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-01 20:43:01 +0530

    Update marblingsorter.py

--- marblingsorter.py
+++ marblingsorter.py
@@ -26,7 +26,7 @@ def import_and_predict(image_data, model):
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
         
         img_reshape = img[np.newaxis,...]
     
