5f6109e4ec1120ae45db797e48c8490e0c6c158f
AnuhasNiluminda18 <119007959+AnuhasNiluminda18@users.noreply.github.com>
2022-12-02 06:04:29 +0530

    Create marblingsorter.py

--- marblingsorter.py
+++ marblingsorter.py
@@ -1,29 +1,17 @@
 import streamlit as st
 import tensorflow as tf
 import streamlit as st
-from pathlib import Path
-
 
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-  save_dest = Path('model')
-  save_dest.mkdir(exist_ok=True)
-    
-  f_checkpoint = Path("model/content/drive/MyDrive/code/model_save/my_model1.hdf5")
-
-  # with st.spinner("Downloading model... this may take awhile! \n Don't stop it!"):
-        # from GD_download import download_file_from_google_drive
-         #download_file_from_google_drive(cloud_model_location, f_checkpoint)
-    
-  model = torch.load(f_checkpoint, map_location=device)
-  model.eval()
+  model=tf.keras.models.load_model('/content/drive/MyDrive/code/model_save/my_model.hdf5')
   return model
 with st.spinner('Model is being loaded..'):
   model=load_model()
 
 st.write("""
-         # Beef marbling Classification
+         # bEEF Classification
          """
          )
 
@@ -38,7 +26,7 @@ def import_and_predict(image_data, model):
         image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
         image = np.asarray(image)
         img = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
+        #img_resize = (cv2.resize(img, dsize=(75, 75),    interpolation=cv2.INTER_CUBIC))/255.
         
         img_reshape = img[np.newaxis,...]
     
